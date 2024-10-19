7feea556632c0403b8e984acbb3672259d11dfb5
Вова Герман <70112136+Germanchuk@users.noreply.github.com>
2024-10-19 18:27:46 +0300

    3 cols

--- src/components/Song/SongSections/SongSections.tsx
+++ src/components/Song/SongSections/SongSections.tsx
@@ -5,8 +5,15 @@ import MagicInput from "../MagicInput/MagicInput";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { v4 as uuidv4 } from "uuid";
 import Section from "./Section/Section";
+import Masonry from "react-masonry-css";
 
 export default function SongSections({ song, setSong, editMode = false }) {
+  const breakpointColumnsObj = {
+    default: 3, // 3 columns by default (you can adjust this as needed)
+    1100: 2, // 2 columns when the screen width is <= 1100px
+    700: 1, // 1 column when the screen width is <= 700px
+  };
+
   const getBlockChanger = (blockIndex) => (newValue) => {
     setSong((song) => {
       const updatedBlocks = [...song.content];
@@ -48,24 +55,30 @@ export default function SongSections({ song, setSong, editMode = false }) {
   }
 
   return (
-    <div className="flex flex-col gap-4">
-      {song.content.map((section, index) => {
-        return (
-          <>
-            <Section
-              id={section.id}
-              value={section.content}
-              setValue={getBlockChanger(index)}
-              deleteBlock={getBlockRemover(index)}
-              addBlockBelow={addBlockBelow(index)}
-              key={section.id}
-              // wrapperClassName="py-2"
-              className="text-lg"
-              editMode={editMode}
-            />
-          </>
-        );
-      })}
+    <div className="max-h-96">
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {song.content.map((section, index) => {
+          return (
+            <>
+              <Section
+                id={section.id}
+                value={section.content}
+                setValue={getBlockChanger(index)}
+                deleteBlock={getBlockRemover(index)}
+                addBlockBelow={addBlockBelow(index)}
+                key={section.id}
+                // wrapperClassName="py-2"
+                className="text-lg py-6"
+                editMode={editMode}
+              />
+            </>
+          );
+        })}
+      </Masonry>
     </div>
   );
 }
