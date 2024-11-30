11ca01d3011649756814bf6416f30add8f9c19c8
Вова Герман <70112136+Germanchuk@users.noreply.github.com>
2024-11-30 11:50:16 +0200

    update logo

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,36 +1,35 @@
 export enum Routes {
-    Root = "/",
-    Login = "/login",
-    Register = "/register",
-    Preferences = "/preferences",
-    //
-    PublicSongs = "/publicSongs",
-    SinglePublicSong = "/publicSongs/:songId",
-    EditPublicSong = "/publicSongs/edit/:songId",
-    //
-    ChurchSongs = "/churchSongs",
-    SingleChurchSong = "/churchSongs/:songId",
-    EditChurchSong = "/churchSongs/edit/:songId",
-    //
-    BandSongs = "/bandSongs",
-    SingleBandSong = "/bandSongs/:songId",
-    EditBandSong = "/bandSongs/edit/:songId",
-    //
-    CreateSong = "/songs/create",
-    AddSongTextToSong = "/songs/create/textToSong",
-    AddSongFromScratch = "/songs/create/fromScratch",
-    //
-    ChurchShedule = "/churchShedule",
-    BandShedule = "/bandShedule",
-    CreateBandShedule = "/bandShedule/create",
-    //
-    JoinChurch = "/churches",
-    CreateChurch = "/churches/add",
-    SingleChurch = "/churches/:churchId",
-    //
-    JoinBand = "/bands",
-    CreateBand = "/bands/add",
-    SingleBand = "/bands/:bandId",
-    //
-
+  Root = "/",
+  Login = "/login",
+  Register = "/register",
+  Preferences = "/preferences",
+  //
+  PublicSongs = "/publicSongs",
+  SinglePublicSong = "/publicSongs/:songId",
+  EditPublicSong = "/publicSongs/edit/:songId",
+  //
+  ChurchSongs = "/churchSongs",
+  SingleChurchSong = "/churchSongs/:songId",
+  EditChurchSong = "/churchSongs/edit/:songId",
+  //
+  BandSongs = "/bandSongs",
+  SingleBandSong = "/bandSongs/:songId",
+  EditBandSong = "/bandSongs/edit/:songId",
+  //
+  CreateSong = "/songs/create",
+  AddSongTextToSong = "/songs/create/textToSong",
+  AddSongFromScratch = "/songs/create/fromScratch",
+  //
+  ChurchShedule = "/churchShedule",
+  BandShedule = "/bandShedule",
+  CreateBandShedule = "/bandShedule/create",
+  //
+  JoinChurch = "/churches",
+  CreateChurch = "/churches/add",
+  SingleChurch = "/churches/:churchId",
+  //
+  JoinBand = "/bands",
+  CreateBand = "/bands/add",
+  SingleBand = "/bands/:bandId",
+  //
 }
